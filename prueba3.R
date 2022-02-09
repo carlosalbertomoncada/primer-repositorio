af754cf30258990e7ace270890195ee57cdc8009
carlosalbertomoncada.git <carlosmoncada.vazquez@gmail.com>
2022-02-09 11:13:40 -0600

    ggplot

--- prueba3.R
+++ prueba3.R
@@ -1,5 +1,19 @@
-git config --global user.name "carlosalbertomoncada.git"
-git config --global user.email "carlosmoncada.vazquez@gmail.com"
+#git config --global user.name "carlosalbertomoncada.git"
+#git config --global user.email "carlosmoncada.vazquez@gmail.com"
+
+#TOKEN
+#usethis::create_github_token()
+
+
+#Credenciales
+#gitcreds::gitcreds_set()
+
+
+library(here)
+
+dir.create(here("scripts"))
+dir.create(here("imagenes"))
+dir.create(here("librerias"))
 
 
 
